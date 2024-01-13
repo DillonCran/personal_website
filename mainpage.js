0a3b90e35321a82435f5ecbe8b23b5d410be7a69
Dillon Cran <dillon.cran@gmail.com>
2024-01-13 01:12:49 -0500

    added interests placeholder

--- mainpage.js
+++ mainpage.js
@@ -7,3 +7,16 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+document.addEventListener('DOMContentLoaded', function () {
+  var image = document.getElementById('expandImage');
+  var textBlock = document.getElementById('expandedText');
+
+  image.addEventListener('click', function () {
+      if (textBlock.style.display === 'none') {
+          textBlock.style.display = 'block';
+      } else {
+          textBlock.style.display = 'none';
+      }
+  });
+});
